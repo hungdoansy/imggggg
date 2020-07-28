0385a2527e65cb424df4b4854d42b849aa2d01b3
Jayden <jayden@gotiapp.co>
2020-07-28 16:33:20 +0700

    feat: style the CategoryCell

--- src/components/CategoryGrid/CategoryCell.jsx
+++ src/components/CategoryGrid/CategoryCell.jsx
@@ -1,5 +1,58 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-export const Category = () => {
-  return <p>Category</p>;
+const CategoryCellView = ({ className, id, name, imageUrl, description }) => {
+  return (
+    <div className={className}>
+      <Link to={`/categories/${id}/items`}>
+        <img src={imageUrl} alt={description} />
+      </Link>
+
+      <div>
+        <span>{name}</span>
+      </div>
+    </div>
+  );
 };
+
+export const CategoryCell = styled(CategoryCellView)`
+  position: relative;
+
+  width: 200px;
+  height: 200px;
+  border-radius: 4px;
+
+  > a > img {
+    max-height: 100%;
+    min-width: 100%;
+
+    object-fit: cover;
+    vertical-align: bottom;
+    border-radius: 4px;
+  }
+
+  > div {
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 200px;
+    height: 200px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: rgba(0, 0, 0, 0.3);
+
+    border-radius: 4px;
+
+    > span {
+      color: white;
+      text-transform: capitalize;
+      font-size: 24px;
+      font-weight: bold;
+    }
+  }
+`;
--- src/components/CategoryGrid/CategoryGrid.jsx
+++ src/components/CategoryGrid/CategoryGrid.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+import { CategoryCell } from "./CategoryCell";
 
-export const CategoryGrid = () => {
-  return <p>CategoryGrid</p>;
+const CategoryGridView = ({ className }) => {
+  return (
+    <div className={"Container Container--fluid " + className}>
+      <CategoryCell
+        imageUrl={
+          "https://images.unsplash.com/photo-1595831888313-fb478bd3dbf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+        }
+        id={1}
+        name="berries"
+        description={"Berries"}
+      />
+    </div>
+  );
 };
+
+export const CategoryGrid = styled(CategoryGridView)`
+  margin-left: -6px;
+  margin-right: -6px;
+
+  > ${CategoryCell} {
+    margin-left: 6px;
+    margin-right: 6px;
+  }
+`;
