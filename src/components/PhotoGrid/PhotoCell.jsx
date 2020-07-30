74a06336aef8b39aad6735945b40d0f0bdef92f2
Jayden <jayden@gotiapp.co>
2020-07-30 12:38:58 +0700

    feat(photo cell): style the control buttons

--- src/components/PhotoGrid/PhotoCell.jsx
+++ src/components/PhotoGrid/PhotoCell.jsx
@@ -8,6 +8,7 @@ const RemoveIcon = () => {
       height="24"
       width="24"
       viewBox="0 0 24 24"
+      fill="currentColor"
     >
       <path d="M0 0h24v24H0V0z" fill="none" />
       <path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z" />
@@ -22,6 +23,7 @@ const EditIcon = () => {
       height="24"
       width="24"
       viewBox="0 0 24 24"
+      fill="currentColor"
     >
       <path d="M0 0h24v24H0V0z" fill="none" />
       <path d="M14.06 9.02l.92.92L5.92 19H5v-.92l9.06-9.06M17.66 3c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29zm-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z" />
@@ -29,6 +31,7 @@ const EditIcon = () => {
   );
 };
 
+// TODO: only shows button when hovering the photo
 const PhotoCellView = ({ className, src, alt }) => {
   return (
     <div className={className}>
@@ -43,13 +46,13 @@ const PhotoCellView = ({ className, src, alt }) => {
           </div>
 
           <div className="controls">
-            <div className="control-button edit-button">
+            <button className="control-button edit-button">
               <EditIcon />
-            </div>
+            </button>
 
-            <div className="control-button remove-button">
+            <button className="control-button remove-button">
               <RemoveIcon />
-            </div>
+            </button>
           </div>
         </div>
       </a>
@@ -63,7 +66,7 @@ export const PhotoCell = styled(PhotoCellView)`
   height: 100%;
   min-width: 100%;
 
-  background-color: red;
+  background-color: rgba(255, 255, 255, 0.7);
   position: relative;
 
   transition: all 0.7s ease-out;
@@ -71,6 +74,12 @@ export const PhotoCell = styled(PhotoCellView)`
   > a {
     height: 100%;
     min-width: 100%;
+
+    &:hover {
+      .info-overlay {
+        visibility: visible;
+      }
+    }
   }
 
   .photo {
@@ -84,6 +93,8 @@ export const PhotoCell = styled(PhotoCellView)`
   }
 
   .info-overlay {
+    visibility: hidden;
+
     background-color: rgba(0, 0, 0, 0.2);
     position: absolute;
 
@@ -94,16 +105,48 @@ export const PhotoCell = styled(PhotoCellView)`
     left: 0;
 
     color: white;
-    border: 1px dashed red;
   }
 
   .controls {
     position: absolute;
 
-    bottom: 0;
-    right: 0;
+    bottom: 5px;
+    right: 5px;
 
     display: flex;
     flex-direction: row;
+
+    .control-button {
+      background-color: rgba(255, 255, 255, 0.7);
+      border: none;
+      border-radius: 4px;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      width: 2.2em;
+      height: 2.2em;
+
+      margin-left: 5px;
+      margin-right: 5px;
+
+      cursor: pointer;
+
+      color: #95a5a6;
+
+      > svg {
+        transition: color 0.1s linear;
+        width: 100%;
+        height: 100%;
+        color: inherit;
+      }
+
+      &:hover {
+        > svg {
+          color: white;
+        }
+      }
+    }
   }
 `;
