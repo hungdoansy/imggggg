38567c687992c7a6fbec8dfd2c166cb02ea1fb38
Jayden <jayden@gotiapp.co>
2020-07-30 12:16:49 +0700

    feat(photo cell): add overlay

--- src/components/PhotoGrid/PhotoCell.jsx
+++ src/components/PhotoGrid/PhotoCell.jsx
@@ -2,15 +2,50 @@ import React from "react";
 import styled from "styled-components";
 
 const PhotoCellView = ({ className, src, alt }) => {
-  return <img className={className} alt={alt} src={src}></img>;
+  return (
+    <div className={className}>
+      <a href="/">
+        <img className="photo" alt={alt} src={src} />
+        <div className="info-overlay">Hehe</div>
+      </a>
+    </div>
+  );
 };
 
 export const PhotoCell = styled(PhotoCellView)`
-  max-height: 100%;
+  display: inline-flex;
+
+  height: 100%;
   min-width: 100%;
 
-  object-fit: cover;
-  vertical-align: bottom;
+  background-color: red;
+  position: relative;
 
-  border-radius: 4px;
+  > a {
+    height: 100%;
+    min-width: 100%;
+  }
+
+  .photo {
+    height: 100%;
+    min-width: 100%;
+
+    object-fit: cover;
+    vertical-align: bottom;
+
+    border-radius: 4px;
+  }
+
+  .info-overlay {
+    background-color: rgba(0, 0, 0, 0.2);
+    position: absolute;
+
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+
+    color: white;
+    border: 1px dashed red;
+  }
 `;
