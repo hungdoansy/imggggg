28ae901a8881af5baab80f9c3433c41909cd5dc9
Jayden <jayden@gotiapp.co>
2020-07-31 11:53:13 +0700

    fix(category container): useState and useEffect for page

--- src/components/CategoryContainer/CategoryContainer.jsx
+++ src/components/CategoryContainer/CategoryContainer.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CategoryGrid } from "../CategoryGrid/CategoryGrid";
 import { useHashParams } from "../../utils/hooks";
 import { withRouter } from "react-router";
 
 export const CategoryContainer = withRouter(() => {
   const hashParams = useHashParams();
-  const page = hashParams.getPage();
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    setPage(hashParams.getPage());
+  }, [hashParams]);
 
   return (
     <>
+      <p>Below is categories updated </p>
       <CategoryGrid currentPage={page} />
     </>
   );
