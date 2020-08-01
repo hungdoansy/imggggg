342de702ed7b948475559d4502554b61fec41f28
Jayden <jayden@gotiapp.co>
2020-08-01 15:19:42 +0700

    feat(reducers): remove old category reducer

--- src/reducers/category.js
+++ src/reducers/category.js
@@ -1,35 +1,51 @@
-const {
-  FETCH_CATEGORIES_SUCCESS,
-  CREATE_CATEGORY_SUCCESS,
-} = require("../constants/action.types");
+import { FETCH_CATEGORIES_SUCCESS } from "../constants/action.types";
+import { CATEGORIES_PER_PAGE } from "../constants/settings";
 
 const initialState = {
-  totalCategories: 0,
-  categories: [],
-  categoriesCreatedByThisUser: [],
+  allPageNumbers: [],
+  idsByPageNumber: {},
+  byId: {},
+  totalRemoteCount: 0,
+  totalNumberOfRemotePages: 0,
 };
 
+const updateState = (state, data, extra) => {
+  const totalRemoteCount = data["total_categories"];
+  const totalPages = Math.ceil(totalRemoteCount / CATEGORIES_PER_PAGE);
+  const page = extra["page"];
+  const pageIndex = state.allPageNumbers.indexOf(page);
+
+  // Update the total number of remote pages
+  state.totalNumberOfRemotePages = totalPages;
+
+  // Update the total count from remote
+  state.totalRemoteCount = totalRemoteCount;
+
+  // Update allPageNumbers
+  if (pageIndex === -1) {
+    state.allPageNumbers.push(page);
+    state.allPageNumbers.sort((a, b) => a - b);
+  }
+
+  // Update idsByPageNumber
+  state.idsByPageNumber[page] = data["categories"].map((c) => c.id);
+
+  // Update byId
+  data["categories"].forEach((c) => {
+    state.byId[c.id] = c;
+  });
+};
+
+// category reducer
 const category = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_CATEGORIES_SUCCESS: {
       const nextState = JSON.parse(JSON.stringify(state));
 
-      nextState.categories = action.data["categories"];
-      nextState.totalCategories = action.data["total_categories"];
+      updateState(nextState, action.data, action.extra);
 
       return nextState;
     }
-
-    case CREATE_CATEGORY_SUCCESS: {
-      const nextState = JSON.parse(JSON.stringify(state));
-
-      nextState.categoriesCreatedByThisUser.push(action.data);
-      nextState.totalCategories += 1;
-
-      return nextState;
-    }
-
-    // TODO: POST_CATEGORY_SUCCESS
 
     default: {
       return state;
@@ -37,8 +53,39 @@ const category = (state = initialState, action) => {
   }
 };
 
-const getCategories = (state) => {
-  return state.categoriesCreatedByThisUser.concat(state.categories);
+const getCategoriesByPageNumber = (wholeState, page = 1) => {
+  const state = wholeState.category;
+
+  const doesNotExistOrBeTheLastPage = (page) => {
+    const index = state.allPageNumbers.indexOf(page);
+
+    return (
+      index === -1
+      // ||
+      // index === state.allPageNumbers.length - 1 ||
+      // page === state.totalNumberOfRemotePages
+    );
+  };
+
+  if (doesNotExistOrBeTheLastPage(page)) {
+    return [];
+  } else {
+    const ids = state.idsByPageNumber[page];
+
+    return ids.map((id) => state.byId[id]);
+  }
 };
 
-export { category, getCategories };
+const getTotalNumberOfRemotePages = (wholeState) =>
+  wholeState.category.totalNumberOfRemotePages;
+
+const getTotalNumberOfCategories = (wholeState) =>
+  wholeState.category.totalRemoteCount;
+
+const selectors = {
+  getCategoriesByPageNumber,
+  getTotalNumberOfRemotePages,
+  getTotalNumberOfCategories,
+};
+
+export { category, selectors };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
-import * as fromCategory from "./category";
-import * as fromCategory2 from "./category2";
+import * as fromCategory2 from "./category";
 
 const rootReducer = combineReducers({
   category: fromCategory2.category,
-  // what: fromCategory.category,
 });
 
 const selectors = {
