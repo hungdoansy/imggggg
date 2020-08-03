aff3d8e7b3075c696f81d84c89734e55b633f100
Jayden <jayden@gotiapp.co>
2020-08-03 16:20:49 +0700

    feat(apis): phase in using real api host for sign in/sign up

--- src/utils/apis/auth.js
+++ src/utils/apis/auth.js
@@ -1,38 +1,13 @@
-const users = [{ email: "admin@gotitapp.co", password: "123455" }];
+import axios from "axios";
 
-// TODO: convert to the real API
-const login = ({ email, password }) => {
-  return new Promise((resolve, reject) => {
-    // TODO: Check for 400 and 404 response
-    if (
-      users.find((u) => u.email === email && u.password === password) !== -1
-    ) {
-      setTimeout(() => {
-        resolve({ access_token: "987654" });
-      }, 2000);
-    } else {
-      setTimeout(() => {
-        reject({ message: "Username and password are not matched." });
-      }, 2000);
-    }
-  });
+const API_HOST = process.env.REACT_APP_API_HOST;
+
+const signin = ({ email, password }) => {
+  return axios.post(`${API_HOST}/auth`, { email, password });
 };
 
-const signup = ({ name, email, password }) => {
-  return new Promise((resolve, reject) => {
-    // TODO: Check for 400 and 404 response
-    if (users.find((u) => u.email === email) !== -1) {
-      setTimeout(() => {
-        reject({ message: "Existed" });
-      }, 2000);
-    } else {
-      users.push({ name, email, password });
-
-      setTimeout(() => {
-        resolve({ access_token: "9876543" });
-      }, 2000);
-    }
-  });
+const signup = ({ email, password, name }) => {
+  return axios.post(`${API_HOST}/users`, { email, password, name });
 };
 
-export { login, signup };
+export { signin, signup };
