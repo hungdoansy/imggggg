3bd82f7cccb3f0c94a98f4cb993de918fa0a2829
Jayden <jayden@gotiapp.co>
2020-08-05 14:56:01 +0700

    refactor(store): move middleware to a separate file

--- src/store.js
+++ src/store.js
@@ -1,57 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { requestRegex } from "./constants/action.types";
+
 import { rootReducer } from "./reducers";
-
-const isAsyncAction = (action) => {
-  return (
-    requestRegex.test(action.type) &&
-    !!action.promise &&
-    typeof action.promise.then === "function"
-  );
-};
-
-const getSuccessType = (action) => {
-  return action.type.replace(requestRegex, "_SUCCESS");
-};
-
-const getFailureType = (action) => {
-  return action.type.replace(requestRegex, "_FAILURE");
-};
-
-// TODO: move this to a separate file
-const asyncHandler = (store) => (next) => (action) => {
-  if (isAsyncAction(action)) {
-    // There would be like {type, promise, extra}
-    const { type, promise, ...rest } = action;
-
-    // don't forward the promise
-    next({ type, ...rest });
-
-    // TODO: need return or not?
-    // If using return, we can use const result = dispatch(someAction());
-    action.promise
-      .then((data) => {
-        // return
-        next({
-          type: getSuccessType(action),
-          data,
-          ...rest,
-        });
-      })
-      .catch((error) => {
-        // return
-        next({
-          type: getFailureType(action),
-          error,
-          ...rest,
-        });
-      });
-  } else {
-    // return
-    next(action);
-  }
-};
+import { asyncHandler } from "./middleware";
 
 const devStore = createStore(
   rootReducer,
