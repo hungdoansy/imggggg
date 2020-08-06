41b5310a9789666fa4c1a9ce191a26f0bec40f57
Jayden <jayden@gotiapp.co>
2020-08-06 17:29:12 +0700

    chore: remove unused code

--- src/components/PhotoContainer/components/PhotoGrid/PhotoGrid.jsx
+++ src/components/PhotoContainer/components/PhotoGrid/PhotoGrid.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 import { PhotoCell } from "./PhotoCell";
 
