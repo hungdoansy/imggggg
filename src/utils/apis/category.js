03e5b619da8f8b98dec3bd36a7bfc03b8342f8ee
Jayden <jayden@gotiapp.co>
2020-08-07 22:59:44 +0700

    feat(apis): simplify fetch requests

--- src/utils/apis/category.js
+++ src/utils/apis/category.js
@@ -1,4 +1,5 @@
 import { CATEGORIES_PER_PAGE } from "../../constants/settings";
+import { generateRequest } from "./generateRequest";
 
 const API_HOST = process.env.REACT_APP_API_HOST;
 
@@ -6,48 +7,26 @@ export const createCategory = (
   { name, imageUrl: image_url, description },
   tokens
 ) => {
-  let status = null;
+  const stringifiedBody = JSON.stringify({ name, image_url, description });
 
-  return fetch(`${API_HOST}/categories`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${tokens}`,
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({ name, image_url, description }),
-  })
-    .then((response) => {
-      status = response.status;
-
-      return response.json();
-    })
-    .then((data) => ({ status, data }))
-    .catch((e) => ({ status, data: e })); // TODO: network failure
+  return generateRequest(
+    "POST",
+    `${API_HOST}/categories`,
+    tokens,
+    stringifiedBody
+  );
 };
 
 export const getCategories = (page) => {
   const offset = (page - 1) * CATEGORIES_PER_PAGE;
   const limit = CATEGORIES_PER_PAGE;
 
-  let status = null;
-
-  return fetch(`${API_HOST}/categories?offset=${offset}&limit=${limit}`)
-    .then((response) => {
-      status = response.status;
-      return response.json();
-    })
-    .then((data) => ({ status, data }))
-    .catch((e) => ({ status, data: e })); // TODO: network failure
+  return generateRequest(
+    "GET",
+    `${API_HOST}/categories?offset=${offset}&limit=${limit}`
+  );
 };
 
 export const getCategoryDetail = (categoryId) => {
-  let status = null;
-
-  return fetch(`${API_HOST}/categories/${categoryId}`)
-    .then((response) => {
-      status = response.status;
-      return response.json();
-    })
-    .then((data) => ({ status, data }))
-    .catch((e) => ({ status, data: e })); // TODO: network failure
+  return generateRequest("GET", `${API_HOST}/categories/${categoryId}`);
 };
--- src/utils/apis/photo.js
+++ src/utils/apis/photo.js
@@ -1,28 +1,18 @@
 import { PHOTOS_PER_PAGE } from "../../constants/settings";
+import { generateRequest } from "./generateRequest";
 
 const API_HOST = process.env.REACT_APP_API_HOST;
 
-// TODO: create myFetch
+// TODO: create generateRequest
 
 const getPhotos = (categoryId, page = 1) => {
   const offset = (page - 1) * PHOTOS_PER_PAGE;
   const limit = PHOTOS_PER_PAGE;
 
-  let status = null;
-
-  return fetch(
+  return generateRequest(
+    "GET",
     `${API_HOST}/categories/${categoryId}/items?offset=${offset}&limit=${limit}`
-  )
-    .then((response) => {
-      status = response.status;
-
-      return response.json();
-    })
-    .then((data) => ({ status, data }))
-    .catch((e) => {
-      console.log("Error while fetching photos", e);
-      return [];
-    }); // TODO: network failure
+  );
 };
 
 const submitPhoto = (
@@ -30,23 +20,14 @@ const submitPhoto = (
   { description, imageUrl: image_url },
   tokens
 ) => {
-  let status = null;
+  const stringifiedBody = JSON.stringify({ description, image_url });
 
-  return fetch(`${API_HOST}/categories/${categoryId}/items`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${tokens}`,
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({ image_url, description }),
-  })
-    .then((response) => {
-      status = response.status;
-
-      return response.json();
-    })
-    .then((data) => ({ status, data }))
-    .catch((e) => ({ status, data: e })); // TODO: network failure
+  return generateRequest(
+    "POST",
+    `${API_HOST}/categories/${categoryId}/items`,
+    tokens,
+    stringifiedBody
+  );
 };
 
 const editPhoto = (
@@ -54,54 +35,29 @@ const editPhoto = (
   { id, description, imageUrl: image_url },
   tokens
 ) => {
-  let status = null;
+  const stringifiedBody = JSON.stringify({ image_url, description });
 
-  return fetch(`${API_HOST}/categories/${categoryId}/items/${id}`, {
-    method: "PUT",
-    headers: {
-      Authorization: `Bearer ${tokens}`,
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({ image_url, description }),
-  })
-    .then((response) => {
-      status = response.status;
-
-      return response.json();
-    })
-    .then((data) => ({ status, data }))
-    .catch((e) => ({ status, data: e })); // TODO: network failure
+  return generateRequest(
+    "PUT",
+    `${API_HOST}/categories/${categoryId}/items/${id}`,
+    tokens,
+    stringifiedBody
+  );
 };
 
 const removePhoto = (categoryId, photoId, tokens) => {
-  let status = null;
-
-  return fetch(`${API_HOST}/categories/${categoryId}/items/${photoId}`, {
-    method: "DELETE",
-    headers: {
-      Authorization: `Bearer ${tokens}`,
-      "Content-Type": "application/json;charset=utf-8",
-    },
-  })
-    .then((response) => {
-      status = response.status;
-
-      return response.json();
-    })
-    .then((data) => ({ status, data }))
-    .catch((e) => ({ status, data: e })); // TODO: network failure
+  return generateRequest(
+    "DELETE",
+    `${API_HOST}/categories/${categoryId}/items/${photoId}`,
+    tokens
+  );
 };
 
 const getPhotoDetail = (categoryId, photoId) => {
-  let status = null;
-
-  return fetch(`${API_HOST}/categories/${categoryId}/items/${photoId}`)
-    .then((response) => {
-      status = response.status;
-      return response.json();
-    })
-    .then((data) => ({ status, data }))
-    .catch((e) => ({ status, data: e })); // TODO: network failure
+  return generateRequest(
+    "GET",
+    `${API_HOST}/categories/${categoryId}/items/${photoId}`
+  );
 };
 
 export { getPhotos, submitPhoto, editPhoto, removePhoto, getPhotoDetail };
