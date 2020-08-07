327938f92ae9e9ed95f218413ec0d1b284538aac
Jayden <jayden@gotiapp.co>
2020-08-07 22:07:43 +0700

    feat(validators): add userInfoValidator

--- src/utils/validators/index.js
+++ src/utils/validators/index.js
@@ -69,4 +69,52 @@ const photoValidator = {
   },
 };
 
-export { categoryValidator, photoValidator };
+const userInfoValidator = {
+  name: (value) => {
+    if (value.length < 1 || value.length > 30) {
+      return {
+        passed: false,
+        message: "name should be maximum of 30 characters",
+      };
+    }
+
+    return {
+      passed: true,
+    };
+  },
+  email: (value) => {
+    if (value === null || value === "") {
+      return {
+        passed: false,
+        message: "email cannot be empty",
+      };
+    }
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!emailRegex.test(value)) {
+      return {
+        passed: false,
+        message: "email should have proper format",
+      };
+    }
+
+    return {
+      passed: true,
+    };
+  },
+
+  password: (value) => {
+    if (value.length < 6) {
+      return {
+        passed: false,
+        message: "password should have at least 6 characters",
+      };
+    }
+
+    return {
+      passed: true,
+    };
+  },
+};
+
+export { categoryValidator, photoValidator, userInfoValidator };
