dafd72f7f0473cb959179da7f2bfeb3c3243df61
Jayden <jayden@gotiapp.co>
2020-08-07 16:25:06 +0700

    chore(deps): use fetch instead of axios

--- src/utils/apis/auth.js
+++ src/utils/apis/auth.js
@@ -1,13 +1,39 @@
-import axios from "axios";
-
 const API_HOST = process.env.REACT_APP_API_HOST;
 
 const signin = ({ email, password }) => {
-  return axios.post(`${API_HOST}/auth`, { email, password });
+  let status = null;
+
+  return fetch(`${API_HOST}/auth`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((response) => {
+      status = response.status;
+      return response.json();
+    })
+    .then((data) => ({ status, data }))
+    .catch((e) => ({ status, data: e })); // TODO: network failure
 };
 
 const signup = ({ email, password, name }) => {
-  return axios.post(`${API_HOST}/users`, { email, password, name });
+  let status = null;
+
+  return fetch(`${API_HOST}/users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify({ email, password, name }),
+  })
+    .then((response) => {
+      status = response.status;
+      return response.json();
+    })
+    .then((data) => ({ status, data }))
+    .catch((e) => ({ status, data: e })); // TODO: network failure;
 };
 
 export { signin, signup };
